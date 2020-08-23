929d1c5287bba84629306873496e38b98b9c6bf6
Chris <zari87@live.com>
2020-08-23 09:31:38 -0400

    Feat: Added Stylesheet

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setenterdGoal] = useState('')
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
   return (
-    <View style={{ padding: 50 }}>
-      <View>
-        <TextInput />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Goals" style={styles.input} />
         <Button title="ADD" />
-
       </View>
-
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: "black", borderBottomWidth: 1, fontSize: 30, width: 200
   },
 });
